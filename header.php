2c941d4c9d7f13930751d36318347ae210e926a5
myersg86@gmail.com <myersg86@gmail.com>
2015-05-31 02:46:10 -0700

    Update header.php to display site description metadata

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <?php ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> class="no-js">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title><?php wp_title(); ?></title>
-        <meta name="description" content="">
+        <meta name="description" content="<?php bloginfo('description'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php wp_head(); ?>
     </head>
